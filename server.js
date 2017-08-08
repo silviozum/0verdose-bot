9114032c308ae15aa7e9bbbdb4f77465a091f3c5
Silvio Freitas <silvio@rockthebank.com.br>
2017-08-08 03:45:28 -0300

    add express

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
-'use strict'
-const http = require('http')
-const Bot = require('messenger-bot')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-let bot = new Bot({
-  token: 'EAAGrXoSazs4BAOjsnsS6kILmpZCAcY4oaOaxYjUKjrgogzwLYzogQZCS7bvwTQuQFZADBc1WqpEqZA9mOD6lI0jbxcp7adqlwfuY7YoyeCMZAxqr7WomfZCkGPg1OuqRZAQvNdOocZBd7Td045LG1mnVNGpDitjrOiJO02gKqdJOL5vzQD2ZAk5Pd',
-  verify: 'VERIFY_TOKEN'
-})
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
 
-bot.on('error', (err) => {
-  console.log(err.message)
-})
+/* For Facebook Validation */
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'EAAGrXoSazs4BACYj40rnhMIIiKOiLYF72strzRrDi2zEyrCqAoxDUdmzrOI2JxQ8bHj8qNpk5DwhXzPZCfh1OZAJaSejYoF8G1kvpylAiYopL9EoRZAq0QfrMdnACihGGmhLm5df5GAtButdKgIaQBCG368ZBmF6iPxxZAZCZCWnrdB1DYjNGye') {
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    res.status(403).end();
+  }
+});
 
-bot.on('message', (payload, reply) => {
-  let text = payload.message.text
-
-  bot.getProfile(payload.sender.id, (err, profile) => {
-    if (err) throw err
-
-    reply({ text }, (err) => {
-      if (err) throw err
-
-      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
-    })
-  })
-})
-
-http.createServer(bot.middleware()).listen(3000)
-console.log('0verdose rodando na porta 3000.')
+/* Handling all messenges */
+app.post('/webhook', (req, res) => {
+  console.log(req.body);
+  if (req.body.object === 'page') {
+    req.body.entry.forEach((entry) => {
+      entry.messaging.forEach((event) => {
+        if (event.message && event.message.text) {
+          sendMessage(event);
+        }
+      });
+    });
+    res.status(200).end();
+  }
+});
