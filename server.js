d369e32baade0084deb8b61c109e5928079116d7
Silvio Freitas <silvio@rockthebank.com.br>
2017-08-08 04:14:23 -0300

    configurando rotas

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
+var path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+app.get('/', function(req, res){
+    res.render('index.html');
 });
 
 /* For Facebook Validation */
 app.get('/webhook', (req, res) => {
-  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'EAAGrXoSazs4BACYj40rnhMIIiKOiLYF72strzRrDi2zEyrCqAoxDUdmzrOI2JxQ8bHj8qNpk5DwhXzPZCfh1OZAJaSejYoF8G1kvpylAiYopL9EoRZAq0QfrMdnACihGGmhLm5df5GAtButdKgIaQBCG368ZBmF6iPxxZAZCZCWnrdB1DYjNGye') {
+  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'overcat') {
     res.status(200).send(req.query['hub.challenge']);
   } else {
     res.status(403).end();
@@ -18,16 +19,20 @@ app.get('/webhook', (req, res) => {
 });
 
 /* Handling all messenges */
-app.post('/webhook', (req, res) => {
-  console.log(req.body);
-  if (req.body.object === 'page') {
-    req.body.entry.forEach((entry) => {
-      entry.messaging.forEach((event) => {
-        if (event.message && event.message.text) {
-          sendMessage(event);
-        }
-      });
-    });
-    res.status(200).end();
-  }
+// app.post('/webhook', (req, res) => {
+//   console.log(req.body);
+//   if (req.body.object === 'page') {
+//     req.body.entry.forEach((entry) => {
+//       entry.messaging.forEach((event) => {
+//         if (event.message && event.message.text) {
+//           sendMessage(event);
+//         }
+//       });
+//     });
+//     res.status(200).end();
+//   }
+// });
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
